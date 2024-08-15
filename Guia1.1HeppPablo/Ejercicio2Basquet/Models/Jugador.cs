5120164a83133539820db1cbb22074b99006f53a
PabloHepp <pablonicolashepp@gmail.com>
2024-08-15 17:54:14 -0300

    ejercicio incompleto aún

---
+++ Guia1.1HeppPablo/Ejercicio2Basquet/Models/Jugador.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2Basquet.Models
+{
+    internal class Jugador
+    {
+        private string nombre;
+        private int nroCamiseta;
+        private int puntos[];
+
+        public Jugador(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public int camiseta
+        {
+            get { return camiseta; }
+            set { camiseta = value; }
+        }
+
+
+    }
+}
