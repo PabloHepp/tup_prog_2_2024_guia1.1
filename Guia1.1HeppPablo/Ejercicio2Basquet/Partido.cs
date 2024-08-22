89a79e6b667781a5d5c9c58e59b482be8c2c1825
PabloHepp <pablonicolashepp@gmail.com>
2024-08-22 01:11:17 -0300

    estructura de clases lista para codificar forms

---
+++ Guia1.1HeppPablo/Ejercicio2Basquet/Partido.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2Basquet
+{
+    internal class Partido
+    {
+        Equipo local;
+        Equipo visitante;
+
+        public void AgregarEquipos (Equipo local, Equipo visitante) 
+        {
+            this.local = local;
+            this.visitante = visitante;
+        }
+    }
+}
